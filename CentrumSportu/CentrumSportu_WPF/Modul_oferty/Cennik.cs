80f0b559e78858e589913bd5ace194fe8900edd0
bkosmala <b.kosmala@interia.pl>
2016-12-10 23:29:27 +0100

    Dodanie pierwszych klas modułu oferty

---
+++ CentrumSportu/CentrumSportu_WPF/Modul_oferty/Cennik.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentrumSportu_WPF.Modul_oferty
+{
+    public class Cennik
+    {
+        private Dictionary<string, float> znizki;
+
+
+        public int IdCennika { get; }
+        public float CenaPodstawowa { get; set; }
+        public Dictionary<string,float> Znizki {
+            get { return znizki; } }
+        public string Waluta { get; set; }
+        public string Nazwa { get; set; }
+        public DateTime DataUtworzenia { get; }
+        public DateTime DataModyfikacji { get; set; }
+        public string Opis { get; set; }
+    }
+}
---
+++ CentrumSportu/CentrumSportu_WPF/Modul_oferty/ListaCennikow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentrumSportu_WPF.Modul_oferty
+{
+    public class ListaCennikow
+    {
+        private List<Cennik> cenniki;
+        public List<Cennik> Cenniki
+        {
+            get { return cenniki; }
+        }
+    }
+}
