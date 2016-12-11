043dd9f987757a35a468304b61fc700605af6334
maciejole <elurin16@gmail.com>
2016-12-11 21:06:28 +0100

    utworzenie klas encyjnych i zdefiniowanie właściwości

---
+++ CentrumSportu/CentrumSportu_WPF/Modul_oferty/Przedmiot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentrumSportu_WPF.Modul_oferty
+{
+    class Przedmiot
+    {
+        public int Id { get; }
+        public String Nazwa { get; set; }
+        public Boolean Dostepnosc { get; set; }
+
+    }
+}
